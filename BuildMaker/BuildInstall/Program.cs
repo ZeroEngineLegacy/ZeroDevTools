af05fcf683be32bd534ddcdb0d2f70e309beff6f
ben.strukus <ben.strukus@zeroengine.io>
2013-01-07 14:00:52 -0800

    First commit of BuildMaker.

---
+++ BuildMaker/BuildInstall/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BuildMaker;
+
+namespace BuildInstall
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //InstallBuilder ib = new InstallBuilder();
+      //ib.Run();
+      ReleaseNoteBuilder rnb = new ReleaseNoteBuilder();
+      rnb.Run();
+    }
+  }
+}

128a1c5088f921f8eb02fae2ea0a8569a5f8b7f5
ZeroBuildBot <ZeroBuildBot@zeroengine.io>
2014-04-11 09:03:41 -0700

    Cleaned up SourceIndexer. Add debug info too.

---
+++ SourceIndexer/SourceIndexer/MercurialIndexer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceIndexer
+{
+  class MercurialIndexer
+  {
+    // Get the revision id of the depo.
+    // This is used to know what revision we want to revert each file back to.
+    static String GetCurrentRevision(String depoPath)
+    {
+      ProcessStartInfo info = new ProcessStartInfo();
+      info.Arguments = "--cwd " + depoPath + " tip --template {node}";
+      info.FileName = "hg.exe";
+      info.RedirectStandardOutput = true;
+      info.UseShellExecute = false;
+      Process process = Process.Start(info);
+      String currentRevision = process.StandardOutput.ReadToEnd();
+      process.WaitForExit();
+
+      return currentRevision;
+    }
+
+    public static String BuildSourceIndex(String depoPath, String pdbPath, String pdbStrPath, List<FileInfo> relativeFiles, bool isSilent)
+    {
+      String currentRevision = GetCurrentRevision(depoPath);
+      if (isSilent == false)
+        Console.WriteLine("Embedding revision {0}", currentRevision);
+
+      // Get the current date and time to put into the pdb.
+      String dateString;
+      DateTime date = DateTime.Now;
+      dateString = date.ToString();
+
+      StringBuilder builder = new StringBuilder();
+      builder.AppendLine("SRCSRV: ini ------------------------------------------------");
+      builder.AppendLine("VERSION=1");
+      builder.AppendLine("INDEXVERSION=2");
+      builder.AppendLine("VERCTRL=HG");
+      builder.AppendLine("DATETIME=" + dateString);
+      builder.AppendLine("SRCSRV: variables ------------------------------------------");
+      builder.AppendLine(@"SRCLOCATION=%ZERO_SOURCE%");
+      // The target location to output the file to is whatever root VS decides
+      // then our revision number then the original relative path.
+      builder.AppendLine(@"SRCSRVTRG=%targ%\%var2%\%var3%");
+      // Grab the file of the desired revision from the local depo at SRCLOCATION
+      // and output that to the source target directory location.
+      // Unfortunately mercurial is unable to execute commands on remote repositories...
+      builder.AppendLine("SRCSRVCMD=hg.exe --cwd \"%fnvar%(SRCLOCATION)\" -y cat %var3% -r %var2% > %SRCSRVTRG%");
+      builder.AppendLine("SRCSRV: source files ---------------------------------------");
+      for (int i = 0; i < relativeFiles.Count; ++i)
+      {
+        builder.AppendLine(String.Format("{0}*{1}*{2}", relativeFiles[i].FullPath, currentRevision, relativeFiles[i].RelativePath));
+      }
+      builder.AppendLine("SRCSRV: end ------------------------------------------------");
+
+      return builder.ToString();
+    }
+
+  }
+}
--- SourceIndexer/SourceIndexer/Program.cs
+++ SourceIndexer/SourceIndexer/Program.cs
@@ -6,16 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace SourceIndexer
 {
+  // Simple structure that stores the full path and relative path of a file.
   class FileInfo
   {
     public String FullPath;
     public String RelativePath;
   }
 
-  class Program
+  class SourceIndexer
   {
+    // The pdb contains a list of what files it uses. This can be extracted using srctool.exe.
+    // Note: this list contains files we don't care about (such as new and malloc).
     static String[] GetFileListing(String pdbLocation, String srcsrvLocation)
     {
       ProcessStartInfo info = new ProcessStartInfo();
@@ -31,8 +34,12 @@ namespace ConsoleApplication1
       return files.Split('\n');
     }
 
+    // The file list we get from the pdb contains all files referenced in the project.
+    // This includes new and malloc. Prune the file listing to only those that
+    // share the same root path as our code.
     static List<String> PruneFileListing(String[] files, String rootPath)
     {
+      // Ignore case by just to-lowering everything
       rootPath = rootPath.ToLower();
       List<String> results = new List<String>();
       for(uint i = 0; i < files.Length; ++i)
@@ -41,16 +48,17 @@ namespace ConsoleApplication1
         {
           results.Add(files[i]);
         }
-      
       }
       return results;
     }
 
+    // Get the revision id of the depo.
+    // This is used to know what revision we want to revert each file back to.
     static String GetCurrentRevision(String depoPath)
     {
       ProcessStartInfo info = new ProcessStartInfo();
       info.Arguments = "--cwd " + depoPath + " tip --template {node}";
-      info.FileName = "hg";
+      info.FileName = "hg.exe";
       info.RedirectStandardOutput = true;
       info.UseShellExecute = false;
       Process process = Process.Start(info);
@@ -60,6 +68,9 @@ namespace ConsoleApplication1
       return currentRevision;
     }
 
+    // Unfortunately, the pdb changes the case of the path. Mercurial is case
+    // sensitive with some commands so this poses problems. Luckily windows can
+    // find the file case-insenstive and then reconstruct the original path.
     public static string GetExactPathName(string pathName)
     {
       if (!(File.Exists(pathName) || Directory.Exists(pathName)))
@@ -79,14 +90,20 @@ namespace ConsoleApplication1
       }
     }
 
+    // We need the relative path of the file from the depo's root so that we it
+    // can be copied later (during the source indexing from a crash dump) to
+    // another directory but with the same relative path from the root.
     static List<FileInfo> GetRelativePaths(List<String> files, String depoPath)
     {
       List<FileInfo> relativePaths = new List<FileInfo>();
 
       for(int i = 0; i < files.Count; ++i)
       {
+        // Make sure to trim out any newlines or other characters at the end
         String fileName = files[i].Trim();
+        // Get the exact name so that mercurial can look it up
         String caseSensitiveFileName = GetExactPathName(fileName);
+        // Chop of the beginning of the file path that matches the depo path.
         String relativeFile = caseSensitiveFileName.Substring(depoPath.Length);
         relativeFile = relativeFile.TrimStart('\\');
 
@@ -99,6 +116,7 @@ namespace ConsoleApplication1
       return relativePaths;
     }
 
+    // Simple helper to turn command line arguments into a dictionary lookup.
     static Dictionary<String, String> ParseCommandLineArgs(string[] args)
     {
       Dictionary<String, String> parsedArgs = new Dictionary<String, String>();
@@ -106,11 +124,14 @@ namespace ConsoleApplication1
       int i = 0;
       while(i < args.Length)
       {
+        // If this argument starts with a '-' then check the next argument.
+        // If the next argument doesn't have a '-' then it's the value of this argument.
+        // If it does have a '-' then assume this is a flag is a boolean being set to true.
         String flag = args[i];
         if (flag.StartsWith("-"))
         {
           int nextIndex = i + 1;
-          String val = "";
+          String val = "true";
           if (nextIndex < args.Length && !args[nextIndex].StartsWith("-"))
             val = args[nextIndex];
 
@@ -124,22 +145,55 @@ namespace ConsoleApplication1
       return parsedArgs;
     }
 
+    // Test function to write each file into the pdb. Unfortunately there's no way to extract the files back out.
+    static void WriteEachFileToDataStream(List<FileInfo> relativeFiles, String psbStrLocation, String pdbLocation)
+    {
+      for (int i = 0; i < relativeFiles.Count; ++i)
+      {
+        String relativePath = relativeFiles[i].RelativePath;
+        String fullPath = relativeFiles[i].FullPath;
+
+
+        ProcessStartInfo info = new ProcessStartInfo();
+        info.Arguments = String.Format("-w -p:\"{0}\" -s:{1} -i:\"{2}\"", pdbLocation, relativePath, fullPath);
+        info.FileName = psbStrLocation;
+        info.RedirectStandardOutput = true;
+        info.RedirectStandardError = true;
+        info.UseShellExecute = false;
+        Process process = Process.Start(info);
+        String fileRevision = process.StandardOutput.ReadToEnd();
+        String errs = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+      }
+    }
+
     static void Main(string[] args)
     {
       Dictionary<String, String> parsedArgs = ParseCommandLineArgs(args);
 
       String depoPath = @"C:\BuildBot\slave\Zero\ZeroCore";
-      String pdbLocation = @"C:\BuildBot\slave\Zero\ZeroBuildOut\Out\Win32\Release\ZeroEditor\ZeroEditor.pdb";
-      String srcSrvLocation = @"C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x86\srcsrv";
+      String pdbLocation = "ZeroEditor.pdb";
+      String srcSrvLocation = "srcsrv";
+      String mode = "hg";
 
+      // Should we not print diagnostic messages?
+      bool isSilent = false;
+
+      // Parse the arguments we need
       String outLocation = "";
-      if(parsedArgs.ContainsKey("-depo"))
-        depoPath =parsedArgs["-depo"];
+      if (parsedArgs.ContainsKey("-depo"))
+        depoPath = parsedArgs["-depo"];
       if (parsedArgs.ContainsKey("-pdb"))
         pdbLocation = parsedArgs["-pdb"];
       if (parsedArgs.ContainsKey("-srcsrv"))
         srcSrvLocation = parsedArgs["-srcsrv"];
+      if (parsedArgs.ContainsKey("-mode"))
+        mode = parsedArgs["-mode"];
+      if (parsedArgs.ContainsKey("-silent"))
+        isSilent = true;
 
+      // If the user specifies where to output the text data we need to
+      // save then use that path. Otherwise put it next to the pdb.
       if (parsedArgs.ContainsKey("-out"))
         outLocation = parsedArgs["-out"];
       else
@@ -148,48 +202,46 @@ namespace ConsoleApplication1
         outLocation = Path.Combine(fullPath, "PdbData.txt");
       }
 
-      String psbStrLocation = Path.Combine(srcSrvLocation, "pdbstr.exe");
+      // Some of the srcsrv tools don't work with relative paths.
+      // Convert all of our paths to full paths.
+      depoPath = Path.GetFullPath(depoPath);
+      pdbLocation = Path.GetFullPath(pdbLocation);
+      srcSrvLocation = Path.GetFullPath(srcSrvLocation);
+      outLocation = Path.GetFullPath(outLocation);
 
-      Console.WriteLine("DepoPath: {0}", depoPath);
-      Console.WriteLine("PdbPath: {0}", pdbLocation);
-      Console.WriteLine("SrcSrvLocation: {0}", srcSrvLocation);
-      Console.WriteLine("OutLocation: {0}", outLocation);
+      // Get the path to the tool used to modify the pdb.
+      String pdbStrLocation = Path.Combine(srcSrvLocation, "pdbstr.exe");
 
-      String currentRevision = GetCurrentRevision(depoPath);
-      String[] allFiles = GetFileListing(pdbLocation, srcSrvLocation);
-      List<String> files = PruneFileListing(allFiles, depoPath);
-      List<FileInfo> relativeFiles = GetRelativePaths(files, depoPath);
-      
-
-      StringBuilder builder = new StringBuilder();
-
-      String dateString;
-      DateTime date = DateTime.Now;
-      dateString = date.ToString();
-
-      builder.AppendLine("SRCSRV: ini ------------------------------------------------");
-      builder.AppendLine("VERSION=1");
-      builder.AppendLine("INDEXVERSION=2");
-      builder.AppendLine("VERCTRL=HG");
-      builder.AppendLine("DATETIME=" + dateString);
-      builder.AppendLine("SRCSRV: variables ------------------------------------------");
-      builder.AppendLine(@"SRCLOCATION=%ZERO_SOURCE%");
-      builder.AppendLine(@"SRCSRVTRG=%targ%\%var2%\%var3%");
-      builder.AppendLine("SRCSRVCMD=hg.exe --cwd \"%fnvar%(SRCLOCATION)\" -y cat %var3% -r %var2% > %SRCSRVTRG%");
-      builder.AppendLine("SRCSRV: source files ---------------------------------------");
-      for (int i = 0; i < relativeFiles.Count; ++i)
+      if (isSilent == false)
       {
-        builder.AppendLine(String.Format("{0}*{1}*{2}", relativeFiles[i].FullPath, currentRevision, relativeFiles[i].RelativePath));
+        // For debugging: output what the directory of each path we need is.
+        Console.WriteLine("DepoPath: {0}", depoPath);
+        Console.WriteLine("PdbPath: {0}", pdbLocation);
+        Console.WriteLine("SrcSrvLocation: {0}", srcSrvLocation);
+        Console.WriteLine("OutLocation: {0}", outLocation);
       }
-      builder.AppendLine("SRCSRV: end ------------------------------------------------");
 
-      File.WriteAllText(outLocation, builder.ToString());
+      String[] allFiles = GetFileListing(pdbLocation, srcSrvLocation);
+      if (isSilent == false)
+        Console.WriteLine("Found {0} files in the pdb", allFiles.Length);
+
+      List<String> files = PruneFileListing(allFiles, depoPath);
+      if (isSilent == false)
+        Console.WriteLine("Pruned file listing to {0} files", files.Count);
+
+      List<FileInfo> relativeFiles = GetRelativePaths(files, depoPath);
+
+      String pdbData = "";
+      if (mode == "hg")
+        pdbData = MercurialIndexer.BuildSourceIndex(depoPath, pdbLocation, pdbStrLocation, relativeFiles, isSilent);
+
+      File.WriteAllText(outLocation, pdbData);
 
 
-
+      // Write the text file into the pdb
       ProcessStartInfo info = new ProcessStartInfo();
       info.Arguments = String.Format("-w -p:\"{0}\" -s:srcsrv -i:\"{1}\"", pdbLocation, outLocation);
-      info.FileName = psbStrLocation;
+      info.FileName = pdbStrLocation;
       info.RedirectStandardOutput = true;
       info.RedirectStandardError = true;
       info.UseShellExecute = false;

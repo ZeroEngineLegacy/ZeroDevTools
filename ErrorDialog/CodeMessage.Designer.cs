3dc9480a761d1866d6eb60dbe751f3de78f2c389
joshua.davis <joshua.davis@zeroengine.io>
2015-03-04 11:34:38 -0800

    Found and added the error dialog

---
+++ ErrorDialog/CodeMessage.Designer.cs
@@ -0,0 +1,144 @@
+namespace ErrorDialog
+{
+    partial class CodeMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CodeMessage));
+            this.MessageText = new System.Windows.Forms.TextBox();
+            this.Message = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DebugBreak = new System.Windows.Forms.Button();
+            this.Continue = new System.Windows.Forms.Button();
+            this.DataGrid = new System.Windows.Forms.PropertyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MessageText
+            // 
+            this.MessageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessageText.Location = new System.Drawing.Point(97, 28);
+            this.MessageText.Multiline = true;
+            this.MessageText.Name = "MessageText";
+            this.MessageText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MessageText.Size = new System.Drawing.Size(567, 57);
+            this.MessageText.TabIndex = 3;
+            this.MessageText.TabStop = false;
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(94, 12);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(53, 13);
+            this.Message.TabIndex = 6;
+            this.Message.Text = "Message:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 73);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DebugBreak
+            // 
+            this.DebugBreak.Image = ((System.Drawing.Image)(resources.GetObject("DebugBreak.Image")));
+            this.DebugBreak.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.DebugBreak.Location = new System.Drawing.Point(511, 259);
+            this.DebugBreak.Name = "DebugBreak";
+            this.DebugBreak.Size = new System.Drawing.Size(153, 40);
+            this.DebugBreak.TabIndex = 0;
+            this.DebugBreak.Text = "Debug Break";
+            this.DebugBreak.UseVisualStyleBackColor = true;
+            this.DebugBreak.Click += new System.EventHandler(this.DebugBreak_Click);
+            // 
+            // Continue
+            // 
+            this.Continue.Image = ((System.Drawing.Image)(resources.GetObject("Continue.Image")));
+            this.Continue.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Continue.Location = new System.Drawing.Point(358, 259);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(147, 40);
+            this.Continue.TabIndex = 1;
+            this.Continue.Text = "Continue";
+            this.Continue.UseVisualStyleBackColor = true;
+            this.Continue.Click += new System.EventHandler(this.Continue_Click);
+            // 
+            // DataGrid
+            // 
+            this.DataGrid.CommandsDisabledLinkColor = System.Drawing.Color.Black;
+            this.DataGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DataGrid.Location = new System.Drawing.Point(12, 91);
+            this.DataGrid.Name = "DataGrid";
+            this.DataGrid.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            this.DataGrid.Size = new System.Drawing.Size(652, 162);
+            this.DataGrid.TabIndex = 2;
+            this.DataGrid.TabStop = false;
+            this.DataGrid.ToolbarVisible = false;
+            // 
+            // CodeMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(678, 306);
+            this.Controls.Add(this.DataGrid);
+            this.Controls.Add(this.Continue);
+            this.Controls.Add(this.DebugBreak);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.MessageText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CodeMessage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zero Engine Error";
+            this.TopMost = true;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CodeMessage_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MessageText;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button DebugBreak;
+        private System.Windows.Forms.Button Continue;
+        private System.Windows.Forms.PropertyGrid DataGrid;
+
+    }
+}
---
+++ ErrorDialog/CodeMessage.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ErrorDialog
+{
+    public partial class CodeMessage : Form
+    {
+        class Info
+        {
+            public Info()
+            {
+                string[] commandLine = System.Environment.GetCommandLineArgs();
+
+                expression = "";
+
+                if (commandLine.Length >= 5)
+                {
+                    expression = commandLine[2];
+                    file = commandLine[3];
+                    buildNumber = commandLine[4];
+                    environment = System.Environment.OSVersion.ToString(); 
+                    shortfile = file.Substring( file.LastIndexOf('\\') );
+                }
+
+                if (expression.Length == 0)
+                    expression = "No Expression";
+
+            }
+            private string expression;
+              
+            [Description("Expression that triggered this warning"),CategoryAttribute("Code Information")]
+            public string Expression
+            {
+                get { return expression; }
+                set { expression = value; }
+            }
+
+            private string shortfile;
+            [Description("Code file that generated this warning. Format, FileName:(LineNumber)"), CategoryAttribute("Code Information")]
+            public string File
+            {
+                get { return shortfile; }
+                set { shortfile = value; }
+            }
+            
+            private string file;
+            [Description("Code file that generated this warning. Format, FileName:(LineNumber)"),CategoryAttribute("Code Information")]
+            public string FullPath
+            {
+                get { return file; }
+                set { file = value; }
+            }
+
+            private string buildNumber;
+            [Description("Build number of this executable. Format, Year.Month.Day.Revision.Desc"), CategoryAttribute("Code Information")]
+            public string BuildNumber
+            {
+                get { return buildNumber; }
+                set { buildNumber = value; }
+            }
+
+            private string environment;
+
+            [Description("Run Time Environment"), CategoryAttribute("Code Information")]
+            public string Environment
+            {
+                get { return environment; }
+                set { environment = value; }
+            }
+	
+        };
+
+        public CodeMessage()
+        {
+            InitializeComponent();
+            this.DataGrid.SelectedObject = new Info();
+            string[] commandLine = System.Environment.GetCommandLineArgs();
+            
+            //Command Line 0 is path
+            if (commandLine.Length > 1)
+                this.MessageText.Text = commandLine[1];
+            else
+                this.MessageText.Text = "No Message";
+
+            this.Continue.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
+            this.DebugBreak.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
+        }
+        
+        private void CodeMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                // Backspaces
+                case '\b':
+                    System.Environment.Exit(2);
+                    break;
+                case (char)27:
+                    System.Environment.Exit(2);
+                    break;
+                case 'd':
+                    System.Environment.Exit(1);
+                    break;
+                //default:
+                  //  System.Environment.Exit(1);
+                    //break; 
+            }
+        }
+
+        private void DebugBreak_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+
+        }
+
+        private void Continue_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Log_Click(object sender, EventArgs e)
+        {
+        }
+
+
+    }
+}

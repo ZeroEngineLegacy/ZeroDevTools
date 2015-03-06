dc98e4a555d6300076c7e47f97f0c7b4336e59cd
joshua.davis <joshua.davis@zeroengine.io>
2015-03-06 08:22:16 -0800

    Fixed the error dialog not compiling and crashing if there were no '/' in the path

--- ErrorDialog/CodeMessage.cs
+++ ErrorDialog/CodeMessage.cs
@@ -8,11 +8,11 @@ using System.Windows.Forms;
 
 namespace ErrorDialog
 {
-    public partial class CodeMessage : Form
+  public partial class CodeMessage : Form
+  {
+    class Info
     {
-        class Info
-        {
-            public Info()
+      public Info()
             {
                 string[] commandLine = System.Environment.GetCommandLineArgs();
 
@@ -23,109 +23,113 @@ namespace ErrorDialog
                     expression = commandLine[2];
                     file = commandLine[3];
                     buildNumber = commandLine[4];
-                    environment = System.Environment.OSVersion.ToString(); 
-                    shortfile = file.Substring( file.LastIndexOf('\\') );
+                    environment = System.Environment.OSVersion.ToString();
+
+                    shortfile = file;
+                    int index = file.LastIndexOf('\\');
+                    if(index != -1)
+                      shortfile = file.Substring(index + 1);
                 }
 
                 if (expression.Length == 0)
                     expression = "No Expression";
 
             }
-            private string expression;
-              
-            [Description("Expression that triggered this warning"),CategoryAttribute("Code Information")]
-            public string Expression
-            {
-                get { return expression; }
-                set { expression = value; }
-            }
+      private string expression;
 
-            private string shortfile;
-            [Description("Code file that generated this warning. Format, FileName:(LineNumber)"), CategoryAttribute("Code Information")]
-            public string File
-            {
-                get { return shortfile; }
-                set { shortfile = value; }
-            }
-            
-            private string file;
-            [Description("Code file that generated this warning. Format, FileName:(LineNumber)"),CategoryAttribute("Code Information")]
-            public string FullPath
-            {
-                get { return file; }
-                set { file = value; }
-            }
+      [Description("Expression that triggered this warning"), CategoryAttribute("Code Information")]
+      public string Expression
+      {
+        get { return expression; }
+        set { expression = value; }
+      }
 
-            private string buildNumber;
-            [Description("Build number of this executable. Format, Year.Month.Day.Revision.Desc"), CategoryAttribute("Code Information")]
-            public string BuildNumber
-            {
-                get { return buildNumber; }
-                set { buildNumber = value; }
-            }
+      private string shortfile;
+      [Description("Code file that generated this warning. Format, FileName:(LineNumber)"), CategoryAttribute("Code Information")]
+      public string File
+      {
+        get { return shortfile; }
+        set { shortfile = value; }
+      }
 
-            private string environment;
+      private string file;
+      [Description("Code file that generated this warning. Format, FileName:(LineNumber)"), CategoryAttribute("Code Information")]
+      public string FullPath
+      {
+        get { return file; }
+        set { file = value; }
+      }
 
-            [Description("Run Time Environment"), CategoryAttribute("Code Information")]
-            public string Environment
-            {
-                get { return environment; }
-                set { environment = value; }
-            }
-	
-        };
+      private string buildNumber;
+      [Description("Build number of this executable. Format, Year.Month.Day.Revision.Desc"), CategoryAttribute("Code Information")]
+      public string BuildNumber
+      {
+        get { return buildNumber; }
+        set { buildNumber = value; }
+      }
 
-        public CodeMessage()
-        {
-            InitializeComponent();
-            this.DataGrid.SelectedObject = new Info();
-            string[] commandLine = System.Environment.GetCommandLineArgs();
-            
-            //Command Line 0 is path
-            if (commandLine.Length > 1)
-                this.MessageText.Text = commandLine[1];
-            else
-                this.MessageText.Text = "No Message";
+      private string environment;
 
-            this.Continue.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
-            this.DebugBreak.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
-        }
-        
-        private void CodeMessage_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            switch (e.KeyChar)
-            {
-                // Backspaces
-                case '\b':
-                    System.Environment.Exit(2);
-                    break;
-                case (char)27:
-                    System.Environment.Exit(2);
-                    break;
-                case 'd':
-                    System.Environment.Exit(1);
-                    break;
-                //default:
-                  //  System.Environment.Exit(1);
-                    //break; 
-            }
-        }
+      [Description("Run Time Environment"), CategoryAttribute("Code Information")]
+      public string Environment
+      {
+        get { return environment; }
+        set { environment = value; }
+      }
 
-        private void DebugBreak_Click(object sender, EventArgs e)
-        {
-            System.Environment.Exit(1);
+    };
 
-        }
+    public CodeMessage()
+    {
+      InitializeComponent();
+      this.DataGrid.SelectedObject = new Info();
+      string[] commandLine = System.Environment.GetCommandLineArgs();
 
-        private void Continue_Click(object sender, EventArgs e)
-        {
-            System.Environment.Exit(0);
-        }
+      //Command Line 0 is path
+      if (commandLine.Length > 1)
+        this.MessageText.Text = commandLine[1];
+      else
+        this.MessageText.Text = "No Message";
 
-        private void Log_Click(object sender, EventArgs e)
-        {
-        }
+      this.Continue.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
+      this.DebugBreak.KeyPress += new KeyPressEventHandler(CodeMessage_KeyPress);
+    }
 
+    private void CodeMessage_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      switch (e.KeyChar)
+      {
+        // Backspaces
+        case '\b':
+          System.Environment.Exit(2);
+          break;
+        case (char)27:
+          System.Environment.Exit(2);
+          break;
+        case 'd':
+          System.Environment.Exit(1);
+          break;
+        //default:
+        //  System.Environment.Exit(1);
+        //break; 
+      }
+    }
+
+    private void DebugBreak_Click(object sender, EventArgs e)
+    {
+      System.Environment.Exit(1);
 
     }
+
+    private void Continue_Click(object sender, EventArgs e)
+    {
+      System.Environment.Exit(0);
+    }
+
+    private void Log_Click(object sender, EventArgs e)
+    {
+    }
+
+
+  }
 }
